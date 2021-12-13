ff23ce510e6969724d8b22c6e63571c19f357805
MatAff <mathijsaffourtit@gmail.com>
2021-12-13 08:06:54 -0700

    Adding day 10

--- day_10.py
+++ day_10.py
@@ -11,21 +11,31 @@ comp_dict = {")": 1, "]": 2, "}": 3, ">": 4}
 total = 0
 comps = []
 for line in input:
+    print(line)
     q = []
+    valid = True
     for c in line:
         if c in valid_dict.keys():
             q.append(c)
         else:
             prev = q.pop()
             if valid_dict[prev] != c:
+                valid = False
                 total = total + val_dict[c]
-    if len(q) > 0:
-        print(q)
+    print(valid)
+    if len(q) > 0 and valid:
+        print(line)
+        print("".join(q))
         score = 0
         while len(q) > 0:
             c = q.pop()
-            score = score * 5 + comp_dict[valid_dict[c]]
+            score = (score) * 5 + comp_dict[valid_dict[c]]
         comps.append(score)
 print(total)
 print(len(comps))
-print(sorted(comps)[int(len(comps)/2)])
+comps = sorted(comps)
+print(comps)
+print(sorted(comps)[(int(len(comps)/2)) + 0])
+
+# 2857960589
+# 2867760472
